203c6964615bc636f7de7ba60fd56de14cc2eac2
Marcin Kulwicki <marcin@MacBook-Pro-Marcin.local>
2019-04-03 00:49:17 +0200

    Photon cd. conf + LogScreen

--- Assets/Scripts/PlayFabAuth.cs
+++ Assets/Scripts/PlayFabAuth.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+
 
 public class PlayFabAuth : MonoBehaviour
 {
+    public TMP_InputField user;
+    public TMP_InputField password;
+    public TMP_InputField email;
+    public bool isAuthenticated = false;
+    private LoginWithPlayFabRequest loginWithPlayFabRequest;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +24,21 @@ public class PlayFabAuth : MonoBehaviour
     void Update()
     {
         
+    }
+    public void LogIn(){
+        loginWithPlayFabRequest = new LoginWithPlayFabRequest();
+        loginWithPlayFabRequest.Username = user.text;
+        loginWithPlayFabRequest.Password = password.text;
+
+        PlayFabClientAPI.LoginWithPlayFab(loginWithPlayFabRequest, result => {
+            //If acc found
+            isAuthenticated = true;
+            Debug.Log("You're logged in");
+        }, error => {
+            //If not found
+            isAuthenticated = false;
+            Debug.Log(error.ErrorMessage);
+
+        },null);
     }
 }
