8b8acc7f77fb5dac34499f5c784edac28b82499f
Marcin Kulwicki <marcin@MacBook-Pro-Marcin.local>
2019-04-03 21:53:11 +0200

    Register

--- Assets/Scripts/PlayFabAuth.cs
+++ Assets/Scripts/PlayFabAuth.cs
@@ -12,12 +12,13 @@ public class PlayFabAuth : MonoBehaviour
     public TMP_InputField user;
     public TMP_InputField password;
     public TMP_InputField email;
+    public TextMeshProUGUI message;
     public bool isAuthenticated = false;
     private LoginWithPlayFabRequest loginWithPlayFabRequest;
     // Start is called before the first frame update
     void Start()
     {
-        
+        email.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -32,13 +33,31 @@ public class PlayFabAuth : MonoBehaviour
 
         PlayFabClientAPI.LoginWithPlayFab(loginWithPlayFabRequest, result => {
             //If acc found
+            message.text = "Welcome "+ user.text+" Connecting...";
             isAuthenticated = true;
             Debug.Log("You're logged in");
         }, error => {
             //If not found
             isAuthenticated = false;
-            Debug.Log(error.ErrorMessage);
+            email.gameObject.SetActive(true);
+
+            message.text = "Failed to log in your account! ["+error.ErrorMessage+"]";  
 
         },null);
+    }
+
+    public void Register(){
+        RegisterPlayFabUserRequest request = new RegisterPlayFabUserRequest();
+        request.Email = email.text;
+        request.Username = user.text;
+        request.Password = password.text;
+
+        PlayFabClientAPI.RegisterPlayFabUser(request, result => {
+
+            message.text = "Your account has been created!";
+        }, error =>{
+            email.gameObject.SetActive(true);
+            message.text = "Please enter your email";    
+        });
     }
 }
