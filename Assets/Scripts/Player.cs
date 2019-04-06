82c5e5b98c5bafa7c60400d003780eb28742384a
Marcin Kulwicki <marcin@MacBook-Pro-Marcin.local>
2019-04-06 21:08:44 +0200

    health and damage

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,23 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon;
+using TMPro;
 
 public class Player : Photon.PunBehaviour
 {
+    public TextMeshProUGUI healthText;
+    //BODY
     public Rigidbody rg;
     public float speed; 
     private float x;
     private float z;
+    //HEALTH
+    private float _health;
+    private float _maxHealth = 100;
+    private float _minHealth = 0;
     // Start is called before the first frame update
     void Start(){
 
         rg = GetComponent<Rigidbody>();
+        _health = _maxHealth;
 
         if (photonView.isMine){
             
         }else{
             
         }
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "Hurt"){
+
+            if (photonView.isMine){
+                
+                photonView.RPC("Damage", PhotonTargets.All);
+            }
+        }
+    }
+
+    [PunRPC]
+    private void Damage(){
+        _health -= 10;
     }
 
     // Update is called once per frame
@@ -28,6 +51,13 @@ public class Player : Photon.PunBehaviour
 
             x = Input.GetAxisRaw("Horizontal");
             z = Input.GetAxisRaw("Vertical");
+
+            if(_health > _maxHealth){
+                _health = _maxHealth;
+            } 
+            if(_health < _minHealth) {
+                _health = _minHealth;
+            }
         }else{
             
         }
@@ -35,11 +65,25 @@ public class Player : Photon.PunBehaviour
 
     private void FixedUpdate() {
         
+        healthText.text = _health.ToString();
         if (photonView.isMine){
 
             rg.AddForce(new Vector3(x,0,z) * speed);
         }else{
             
+        }
+    }
+
+    public virtual void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info){
+
+        if(stream.isWriting){
+            
+            //Mine component
+            stream.SendNext(_health);
+        }else if(stream.isReading){
+
+            //Their component
+            _health = (float) stream.ReceiveNext();
         }
     }
 }
